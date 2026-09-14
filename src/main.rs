8e48061866810852220c8baf2741f4a0a0682746
Kayra <kayra19206@gmail.com>
2026-09-14 19:39:20 +1000

    feat: basic app setup

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::io;
+
+use crossterm::event::{KeyCode, KeyEventKind};
+use ratatui::{DefaultTerminal, Frame, style::Stylize, text::Line, widgets::Widget};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+
+    let mut app = App { exit: false };
+
+    let app_result = app.run(&mut terminal);
+
+    ratatui::restore();
+    app_result
+}
+
+pub struct App {
+    exit: bool,
+}
+
+impl App {
+    fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            match crossterm::event::read()? {
+                crossterm::event::Event::Key(key_event) => self.handle_key_event(key_event)?,
+                _ => {}
+            }
+            terminal.draw(|frame| self.draw(frame))?;
+        }
+
+        Ok(())
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_key_event(&mut self, key_event: crossterm::event::KeyEvent) -> io::Result<()> {
+        if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('q') {
+            self.exit = true;
+        }
+        Ok(())
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
+    where
+        Self: Sized,
+    {
+        Line::from("Process overview").bold().render(area, buf);
+    }
+}
