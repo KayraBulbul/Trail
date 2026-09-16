e3ff2bd82b0046e1dd89a2190ef0cd18e9a811df
Kayra <kayra19206@gmail.com>
2026-09-16 22:56:58 +1000

    feat: input box and getting user input

---
+++ src/input.rs
@@ -0,0 +1,72 @@
+pub struct Input {
+    pub input: String,
+    pub character_index: usize,
+    pub input_mode: InputMode,
+}
+
+#[derive(PartialEq)]
+pub enum InputMode {
+    Normal,
+    Editing,
+}
+
+impl Input {
+    pub const fn new() -> Self {
+        Self {
+            input: String::new(),
+            input_mode: InputMode::Normal,
+            character_index: 0,
+        }
+    }
+
+    pub fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    pub fn move_cursor_right(&mut self) {
+        let cursor_moved_right = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_right);
+    }
+
+    pub fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    pub fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn clamp_cursor(&self, new_cursor_pos: usize) -> usize {
+        new_cursor_pos.clamp(0, self.input.chars().count())
+    }
+
+    const fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+
+    pub fn submit_project(&mut self) {
+        self.input.clear();
+        self.reset_cursor();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+mod input;
+
 use std::io;
 
-use crossterm::event::{KeyCode, KeyEventKind};
-use ratatui::{DefaultTerminal, Frame, style::Stylize, text::Line, widgets::Widget};
+use crossterm::event::{Event, KeyCode, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    layout::{Constraint, Layout, Position},
+    style::{Color, Modifier, Style, Stylize},
+    text::{Line, Text},
+    widgets::{Block, Paragraph},
+};
+
+use crate::input::InputMode;
 
 fn main() -> io::Result<()> {
     let mut terminal = ratatui::init();
 
-    let mut app = App { exit: false };
+    let mut app = App {
+        show_project_input: false,
+        exit: false,
+    };
 
     let app_result = app.run(&mut terminal);
 
@@ -15,16 +28,19 @@ fn main() -> io::Result<()> {
 }
 
 pub struct App {
+    show_project_input: bool,
     exit: bool,
 }
 
 impl App {
     fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        let mut text_in = input::Input::new();
         while !self.exit {
-            terminal.draw(|frame| self.draw(frame))?;
-
+            if self.show_project_input {
+                self.new_project(terminal, &mut text_in)?;
+            }
             match crossterm::event::read()? {
-                crossterm::event::Event::Key(key_event) => self.handle_key_event(key_event)?,
+                Event::Key(key_event) => self.handle_key_event(key_event, &mut text_in)?,
                 _ => {}
             }
         }
@@ -32,23 +48,91 @@ impl App {
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
-    }
-
-    fn handle_key_event(&mut self, key_event: crossterm::event::KeyEvent) -> io::Result<()> {
-        if key_event.kind == KeyEventKind::Press && key_event.code == KeyCode::Char('q') {
-            self.exit = true;
+    fn handle_key_event(
+        &mut self,
+        key_event: crossterm::event::KeyEvent,
+        text_in: &mut input::Input,
+    ) -> io::Result<()> {
+        if key_event.kind == KeyEventKind::Press {
+            if self.show_project_input && text_in.input_mode == InputMode::Editing {
+                match key_event.code {
+                    KeyCode::Enter => text_in.submit_project(),
+                    KeyCode::Char(to_insert) => text_in.enter_char(to_insert),
+                    KeyCode::Backspace => text_in.delete_char(),
+                    KeyCode::Left => text_in.move_cursor_left(),
+                    KeyCode::Right => text_in.move_cursor_right(),
+                    KeyCode::Esc => text_in.input_mode = InputMode::Normal,
+                    _ => {}
+                }
+            } else {
+                match key_event.code {
+                    KeyCode::Char('a') => {
+                        self.show_project_input = true;
+                    }
+                    KeyCode::Char('e') => text_in.input_mode = InputMode::Editing,
+                    KeyCode::Char('q') => self.exit = true,
+                    _ => {}
+                }
+            }
         }
         Ok(())
     }
-}
 
-impl Widget for &App {
-    fn render(self, area: ratatui::prelude::Rect, buf: &mut ratatui::prelude::Buffer)
-    where
-        Self: Sized,
-    {
-        Line::from("Process overview").bold().render(area, buf);
+    fn new_project(
+        &self,
+        terminal: &mut DefaultTerminal,
+        text_in: &mut input::Input,
+    ) -> io::Result<()> {
+        terminal.draw(|frame| self.render(frame, text_in))?;
+
+        Ok(())
+    }
+
+    fn render(&self, frame: &mut Frame, text_in: &mut input::Input) {
+        let layout = Layout::vertical([Constraint::Length(1), Constraint::Length(3)]);
+        let [help_area, input_area] = frame.area().layout(&layout);
+
+        let (msg, style) = match text_in.input_mode {
+            InputMode::Normal => (
+                vec![
+                    "Press ".into(),
+                    "q".bold(),
+                    " to exit, ".into(),
+                    "e".bold(),
+                    " to start editing.".bold(),
+                ],
+                Style::default().add_modifier(Modifier::RAPID_BLINK),
+            ),
+            InputMode::Editing => (
+                vec![
+                    "Press ".into(),
+                    "Esc".bold(),
+                    " to stop editing, ".into(),
+                    "Enter".bold(),
+                    " to record the message".into(),
+                ],
+                Style::default(),
+            ),
+        };
+
+        let text = Text::from(Line::from(msg)).patch_style(style);
+        let help_message = Paragraph::new(text);
+        frame.render_widget(help_message, help_area);
+
+        let input = Paragraph::new(text_in.input.as_str())
+            .style(match text_in.input_mode {
+                InputMode::Normal => Style::default(),
+                InputMode::Editing => Style::default().fg(Color::Yellow),
+            })
+            .block(Block::bordered().title("Project Name"));
+        frame.render_widget(input, input_area);
+        match text_in.input_mode {
+            InputMode::Normal => {}
+            #[expect(clippy::cast_possible_truncation)]
+            InputMode::Editing => frame.set_cursor_position(Position::new(
+                input_area.x + text_in.character_index as u16 + 1,
+                input_area.y + 1,
+            )),
+        }
     }
 }
